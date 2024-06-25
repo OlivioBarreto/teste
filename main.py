f19477bc7650614fc2c519c0fd7dd304fe7d9e75
Olivio Barreto <oliviobarreto2007@outlook.com>
2024-06-25 02:53:27 -0300

    Candidatura

---
+++ main.py
@@ -0,0 +1,36 @@
+from time import sleep
+
+print("Faça o seu voto consciente!")
+
+jair = 22
+olivio = 17
+branco = 999
+nulo = 0
+
+while True:
+    # pergunta ao usuario qual candidato ele irá votar
+    user = int(input("Digite o numero do seu candidato: "))
+    if user == 22:
+        jair += 1
+    elif user == 17:
+        olivio += 1
+    elif user == 999:
+        branco += 1
+    elif user == 0:
+        nulo += 1
+    else:
+        print("Candidato não conhecido!")
+        break
+    computer = str(input("Quer continuar?[S/N] ")).upper()
+    if computer == 'N':
+        if computer not in 'N' or computer not in 'S':
+            print("Não entendi sua resposta!")
+            break
+    else:
+        print("Loading...")
+        sleep(2)
+
+print(f"{jair} Pessoas votarem no candidato jair!")
+print(f"{olivio} Pessoas votaram no candidato olivio!")
+print(f"{branco} Pessoas votaram em branco!")
+print(f"{nulo} Pessoas votaram nulo!")
